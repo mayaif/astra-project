b4d5e1c4491d96df8378a4952969b382b601bedf
mayaif <margareta.ifrim@outlook.com>
2024-07-12 21:18:56 +0100

    Create FormField.jsx

---
+++ components/FormField.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setshowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="h-16 bg-black-100 border-black-200 px-4 w-full border-2 rounded-2xl focus:border-secondary items-center">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
