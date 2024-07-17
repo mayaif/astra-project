ccf585f4922376ded166f1326ef1a3df281e15dc
mayaif <margareta.ifrim@outlook.com>
2024-07-17 23:11:42 +0100

    Create SearchInput.jsx

---
+++ components/SearchInput.jsx
@@ -0,0 +1,33 @@
+/** @format */
+
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
+
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setshowPassword] = useState(false);
+  return (
+    <View className="h-16 bg-black-100 border-gray-700 px-4 w-full border-2 rounded-2xl focus:border-secondary flex-row items-center space-x-4">
+      <TextInput
+        className=" text-white font-pregular text-base flex-1 mt-0.5"
+        value={value}
+        placeholder="Search for a video topic"
+        placeholderTextColor="#7b7b8b"
+        onChangeText={handleChangeText}
+        secureTextEntry={title === "Password" && !showPassword}
+      />
+      <TouchableOpacity>
+        <Image source={icons.search} resizeMode="contain" className="w-5 h-5" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
