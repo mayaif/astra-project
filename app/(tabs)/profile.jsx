1fcab76d15af38eae6b142178360bdfea8bc33bc
mayaif <margareta.ifrim@outlook.com>
2024-07-19 17:38:05 +0100

    Update profile.jsx

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,16 +1,60 @@
 /** @format */
 
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "@/components/EmptyState";
+import VideoCard from "@/components/VideoCard";
+import { getUserPosts } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import { icons } from "@/constants";
+import InfoBox from "@/components/InfoBox";
 
 const Profile = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logout = () => {};
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+            <View className="w-16 h-16  rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-16 h-16 rounded-full"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No videos found"
+            subtitle="No video found for this search query"
+          />
+        )}
+      />
+      <StatusBar backgroundColor="#1611622" style="light" />
+    </SafeAreaView>
   );
 };
 
 export default Profile;
-
-const styles = StyleSheet.create({});
