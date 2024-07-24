492237bbed027870fade269526f3525854d670b5
mayaif <margareta.ifrim@outlook.com>
2024-07-24 17:39:01 +0100

    Update home.jsx

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,21 +9,21 @@ import {
   Alert,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
-
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
 import Trending from "@/components/Trending";
 import SearchInput from "@/components/SearchInput";
 import EmptyState from "@/components/EmptyState";
 import VideoCard from "@/components/VideoCard";
+import { useGlobalContext } from "@/context/GlobalProvider";
 import { getAllPosts, getLatestPosts } from "@/lib/appwrite";
 import useAppwrite from "@/lib/useAppwrite";
 
 const Home = () => {
   const { data: posts, refetchData } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
-
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
@@ -43,9 +43,11 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-base text-gray-100">
-                  Welcome back
+                  Welcome back,
                 </Text>
-                <Text className="text-2xl font-psemibold text-white">Mimi</Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {user?.username}
+                </Text>
               </View>
               <View className="mt-1.5">
                 <Image
