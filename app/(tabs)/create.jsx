e5da687ac2abfe17760721726ff3497276bf0847
mayaif <margareta.ifrim@outlook.com>
2024-07-25 21:58:10 +0100

    Update create.jsx

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -11,13 +11,16 @@ import {
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ResizeMode, Video } from "expo-av";
-import * as DocumentPicker from "expo-document-picker";
+import * as ImagePicker from "expo-image-picker";
 import { router } from "expo-router";
 import FormField from "@/components/FormField";
 import CustomButton from "@/components/CustomButton";
 import { icons } from "@/constants";
+import { createVideo } from "@/lib/appwrite";
+import { useGlobalContext } from "@/context/GlobalProvider";
 
 const Create = () => {
+  const { user } = useGlobalContext();
   const [uploading, setUploading] = useState(false);
   const [form, setForm] = useState({
     title: "",
@@ -26,11 +29,14 @@ const Create = () => {
     prompt: "",
   });
   const openPicker = async (selectType) => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type:
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:
         selectType === "image"
-          ? ["image/png", "image/jpg"]
-          : ["video/mp4", "video/gif"],
+          ? ImagePicker.MediaTypeOptions.Images
+          : ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
     if (!result.canceled) {
       if (selectType === "image") {
@@ -39,24 +45,21 @@ const Create = () => {
       if (selectType === "video") {
         setForm({ ...form, video: result.assets[0] });
       }
-    } else {
-      setTimeout(() => {
-        Alert.alert("Document picked", JSON.stringify(result, null, 2));
-      }, 100);
     }
   };
-  const submit = () => {
+  const submit = async () => {
     if (
       form.title === "" ||
       form.video === null ||
-      thumbnail === "" ||
-      prompt === ""
+      form.thumbnail === "" ||
+      form.prompt === ""
     ) {
       Alert.alert("Error", "Please fill in all fields");
       return;
     }
     setUploading(true);
     try {
+      await createVideo({ ...form, userId: user.$id });
       Alert.alert("Success", "Post uploaded successfully");
       router.push("/home");
     } catch (error) {
@@ -86,9 +89,7 @@ const Create = () => {
               <Video
                 source={{ uri: form.video.uri }}
                 className="w-full h-64 rounded-2xl"
-                useNativeControls
                 resizeMode={ResizeMode.COVER}
-                isLooping
               />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
