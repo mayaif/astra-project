02456fa850c10c1fef369acae876073af8c30553
mayaif <margareta.ifrim@outlook.com>
2024-08-06 21:19:51 +0100

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,4 @@
 /** @format */
-import { StyleSheet, Text, View } from "react-native";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
